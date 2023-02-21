165b2448cf67ca25103351f00477762d24359c38
jbbrassoud <jbbrassoud@gmail.com>
2023-02-21 20:33:55 +0100

    media selector OK

--- scripts/factories/mediafactory.js
+++ scripts/factories/mediafactory.js
@@ -1,17 +1,34 @@
 function mediaFactory(dataMedia) {
-    const { name, id, photographerId, title, image, likes, date, price } = dataMedia;
+    const { name, id, photographerId, title, image,video, likes, date, price } = dataMedia;
+    const vidSrc = `assets/${photographerId}/${video}`;
     const imgSrc = `assets/${photographerId}/${image}`;
+    
 
     function getMediaDOM(){
         const div = document.createElement("div");
         div.classList.add("bloc-pic");
         let picImg = document.createElement ('img');
-        picImg.setAttribute("src", imgSrc);
+        let vidImg = document.createElement ('video');
+        
+        if(image){
+            
+            picImg.setAttribute("src", imgSrc);
+            div.appendChild(picImg);
+        }else{
+            
+            vidImg.setAttribute("src", vidSrc);
+            vidImg.setAttribute("type", "video/mp4")
+            vidImg.setAttribute("type", "video/mp4")
+            div.appendChild(vidImg);
+        }
+        
+        
         const h2Title = document.createElement('h2');
         h2Title.textContent = title;
         const hearts = document.createElement("p");
         hearts.textContent = likes;
-        div.appendChild(picImg);
+
+        
         div.appendChild(h2Title);
         div.appendChild(hearts);
 
