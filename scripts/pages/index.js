79d11a38f5d0f1f250ea117c23b7fdda561e479d
jbbrassoud <jbbrassoud@gmail.com>
2023-03-09 14:16:11 +0100

    last

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,7 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        const jsonCall = await fetch('data/photographers.json');
+        const jsonCall = await fetch('/data/photographers.json');
         const photoArray = await jsonCall.json();
         
         return ({
